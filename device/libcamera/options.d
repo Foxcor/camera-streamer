f3dc63f6c6ca73efd06de086143c340db1252ef3
Foxcor <foxcorsoft@bk.ru>
2025-05-29 08:45:01 +0300

    Linux MegaS 6.12.25+rpt-rpi-v8 #1 SMP PREEMPT Debian 1:6.12.25-1+rpt1 (2025-04-30) aarch64

---
+++ device/libcamera/options.d
@@ -0,0 +1,36 @@
+device/libcamera/options.o: device/libcamera/options.cc \
+ device/libcamera/libcamera.hh /home/pi/camera-streamer/version.h \
+ /home/pi/camera-streamer/device/device.h \
+ /home/pi/camera-streamer/device/buffer_list.h \
+ /home/pi/camera-streamer/device/buffer.h \
+ /home/pi/camera-streamer/util/opts/log.h \
+ /home/pi/camera-streamer/util/opts/fourcc.h \
+ /home/pi/camera-streamer/util/opts/control.h \
+ /usr/local/include/libcamera/libcamera/controls.h \
+ /usr/local/include/libcamera/libcamera/base/class.h \
+ /usr/local/include/libcamera/libcamera/base/flags.h \
+ /usr/local/include/libcamera/libcamera/base/span.h \
+ /usr/local/include/libcamera/libcamera/geometry.h \
+ /usr/local/include/libcamera/libcamera/control_ids.h \
+ /usr/local/include/libcamera/libcamera/property_ids.h \
+ /usr/local/include/libcamera/libcamera/libcamera.h \
+ /usr/local/include/libcamera/libcamera/camera.h \
+ /usr/local/include/libcamera/libcamera/base/object.h \
+ /usr/local/include/libcamera/libcamera/base/bound_method.h \
+ /usr/local/include/libcamera/libcamera/base/signal.h \
+ /usr/local/include/libcamera/libcamera/orientation.h \
+ /usr/local/include/libcamera/libcamera/request.h \
+ /usr/local/include/libcamera/libcamera/fence.h \
+ /usr/local/include/libcamera/libcamera/base/unique_fd.h \
+ /usr/local/include/libcamera/libcamera/stream.h \
+ /usr/local/include/libcamera/libcamera/color_space.h \
+ /usr/local/include/libcamera/libcamera/framebuffer.h \
+ /usr/local/include/libcamera/libcamera/base/shared_fd.h \
+ /usr/local/include/libcamera/libcamera/pixel_format.h \
+ /usr/local/include/libcamera/libcamera/camera_manager.h \
+ /usr/local/include/libcamera/libcamera/formats.h \
+ /usr/local/include/libcamera/libcamera/framebuffer_allocator.h \
+ /usr/local/include/libcamera/libcamera/logging.h \
+ /usr/local/include/libcamera/libcamera/transform.h \
+ /usr/local/include/libcamera/libcamera/version.h \
+ /home/pi/camera-streamer/third_party/magic_enum/include/magic_enum.hpp
